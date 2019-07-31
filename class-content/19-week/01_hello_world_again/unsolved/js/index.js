d989a13db32cadafa4f77d26659ec8d47aff6eed
boshengbu <48728617+boshengbu@users.noreply.github.com>
2019-07-30 19:40:51 -0500

    1
    1

--- class-content/19-week/01_hello_world_again/unsolved/js/index.js
+++ class-content/19-week/01_hello_world_again/unsolved/js/index.js
@@ -1,5 +1,12 @@
 // Create a variable below
-
+alert("FUCKIN NORMIES!");
+var helloworld = "Reeeeeeeeeee!!!!!";
 // Create your function here.
 
+function doit() {
+    console.log(helloworld);
+    // Do something here
+}
+
 // Invoke Our function here!
+doit();
--- class-content/19-week/02_understanding_variables/unsolved/js/index.js
+++ class-content/19-week/02_understanding_variables/unsolved/js/index.js
@@ -1,4 +1,16 @@
 // Declare you first function here
+alert("trolololololol!");
+var Pi = 3.1415926;
+var Divisor = 420;   
+var result = Divisor / Pi;
+var ora = "troll"
+var muda ="lololol"
+var stringConcat= ora + " " + muda
 
+function doshit() {
 
+console.log(result);
+console.log(stringConcat);
+}
 // declare you second function here
+doshit();
