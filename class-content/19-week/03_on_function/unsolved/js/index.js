7347893a87541767881d3b8958b4ab87f04737b6
boshengbu <48728617+boshengbu@users.noreply.github.com>
2019-07-31 00:51:17 -0500

    1
    1

--- class-content/19-week/03_on_function/unsolved/js/index.js
+++ class-content/19-week/03_on_function/unsolved/js/index.js
@@ -1,10 +1,17 @@
 // .on event listener documentation - https://api.jquery.com/on/#on-events-selector-data-handler
 
 // Part 1 event Listener Here
-$(' ').on(" ", function(){
-
+$('#button1 ').on("click ", function(){
+$('.hideMe').slideToggle();
+console.log("YOU HAVE TRIGGERED THE KILLER QUEEN THIRD BOMB! BITE ZA DUSTO")
 });
 // Part 2 event listener here
-
+$('#button2').on("click ", function(){
+    $('.cssChange').css('backgroundColor','red')
+    console.log("YOU HAVE TRIGGERED THE KILLER QUEEN THIRD BOMB! BITE ZA DUSTO")
+    });
 // Part 3 event listener here
-
+$('#button3').on("click ", function(){
+    $('.appendMe').append("<div class='child'>You think it's another div but it's me DIO!</div>" )
+    console.log("YOU HAVE TRIGGERED THE KILLER QUEEN THIRD BOMB! BITE ZA DUSTO")
+    });
--- class-content/19-week/04_build_your_own_interaction/unsolved/js/index.js
+++ class-content/19-week/04_build_your_own_interaction/unsolved/js/index.js
@@ -1,1 +1,28 @@
 // Add Event Listener Below
+
+var rotated = false;
+var rainbow = false;
+
+$(".top").on("click", function(){
+    $(".toggle").slideToggle();
+    if(rotated === false) {
+        $(".rotate").css({'transform' : 'rotate(180deg)'});
+        rotated = true;
+    } else {
+        $(".rotate").css({'transform' : 'rotate(0deg)'});
+        rotated = false;
+    }
+    if(rainbow === false) {
+        $(".toggle").css('backgroundImage','linear-gradient(to right, violet, indigo, blue, green, yellow, orange, red)');
+        rainbow = true;
+    } else {
+        $(".toggle").css('backgroundImage','linear-gradient(to right, white, gray)');
+        rainbow = false;
+    }
+    
+ });
+ $('.click').on("click ", function(){
+    $('.wrapper').append("<div class='child'>SUPRISE MUTHAFUKA!</div>" )
+    console.log("YOU HAVE TRIGGERED THE KILLER QUEEN THIRD BOMB! BITE ZA DUSTO")
+    });
+   append=true;
